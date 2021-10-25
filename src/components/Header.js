03a3e072be16d77e2d460d649453747a4bdea2a8
Muhammad Hassan Qureshi <sarafahub@gmail.com>
2021-10-25 18:29:59 +0500

    update

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h2>Expense Tracker</h2>
+      
+      <h3>Muhammad Hassan Qureshi</h3>
+    </div>
+  );
+};
